4ecc49cf07b48ba42c71cb5b42f437553162a65c
petar <petar@LAPTOP-X>
2022-04-17 13:41:57 +0300

    add new project

---
+++ SpecFlowProject2/Pages/CustomersPage.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+
+
+namespace Generali.InsuerWeb.Tests.Pages
+{
+
+    public class CustomersPage : AmexisMainPage
+    {
+        private readonly By versantCorporationLabel = By.XPath("//div[@class='item-content']/h3");
+
+
+        public CustomersPage(RemoteWebDriver driver) : base(driver)
+        {
+        }
+
+        public string VersantCorporationLabelText => Driver.FindElement(versantCorporationLabel).Text;
+
+    }
+}
+
---
+++ SpecFlowProject2/Steps/AmexisSteps.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using Generali.InsuerWeb.Tests.Hooks;
+using Generali.InsuerWeb.Tests.Pages;
+using System.Threading;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowProject2.Steps
+{
+    [Binding]
+    public sealed class AmexisSteps
+    {
+        private AmexisMainPage amexisMainPage;
+        private ContactsPage contactsPage;
+        private CustomersPage customersPage;
+
+        [Given(@"I am on Amexis page")]
+        public void IAmOnAmexisPage()
+        {
+            DriverHooks.Driver.Navigate().GoToUrl("http://www.amexis.net/");
+        }
+
+        [When(@"I open contacts menu from main page")]
+        public void IOpenContactsMenuFromMainPage()
+        {
+            amexisMainPage = new AmexisMainPage(DriverHooks.Driver);
+            amexisMainPage.CLickLang();
+            contactsPage = amexisMainPage.OpenContactsMenu();
+        }
+
+        [When(@"I send the form without entering information")]
+        public void ISendTheFormWithoutEnteringInformation()
+        {
+            contactsPage.ClickSendMessageButton();
+        }
+
+        [Then(@"I should see validation messages")]
+        public void IShouldSeeValidationMessages()
+        {
+            foreach (var element in contactsPage.elementTexts)
+            {
+                element.Text.Should().Be("Полето е задължително.");
+            }
+
+            contactsPage.validationMessageText.Should().Be("Има грешка в някое от полетата. Моля проверете и опитайте отново.");
+        }
+
+        [When(@"I open customer page from main page")]
+        public void IOpenCustomerPageFromMainPage()
+        {
+            amexisMainPage = new AmexisMainPage(DriverHooks.Driver);
+            amexisMainPage.CLickLang();
+            customersPage = amexisMainPage.OpenCustomersMenu();
+        }
+
+        [Then(@"“Versant Corporation” should be present on the customer page")]
+        public void VersantCorporationShouldBePresentOnTheCustomerPage()
+        {
+            customersPage.VersantCorporationLabelText.Should().Contain("Versant");
+        }
+    }
+}
